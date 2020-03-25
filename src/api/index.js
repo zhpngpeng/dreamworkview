a289d82269b7f54564e728962d63575e56f59626
zhpngpeng <zp1994526>
2020-03-25 21:11:31 +0800

    初始化

---
+++ src/api/index.js
@@ -0,0 +1,51 @@
+import Axios from 'axios'
+import router from '../router'
+
+if (window.localStorage.getItem('token')) {
+  Axios.defaults.headers.common['Authorization'] = `Bearer ` + window.localStorage.getItem('token')
+}
+
+export let instance = Axios.create({
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://dreamwork/api' : 'api'
+})
+// request拦截器
+instance.interceptors.request.use(
+  config => {
+    config.headers['Content-Type'] = 'application/json;charset=UTF-8'
+    return config
+  },
+  error => {
+    console.log(error) // for debug
+    Promise.reject(error)
+  }
+)
+// respone拦截器
+instance.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          router.replace({
+            path: 'login',
+            query: { redirect: router.currentRoute.fullPath } // 将跳转的路由path作为参数，登录成功后跳转到该路由
+          })
+      }
+    }
+    return Promise.reject(error.response)
+  }
+)
+
+export const login = ({ loginEmail, loginPassword }) => {
+  let data = JSON.stringify({
+    email: loginEmail,
+    password: loginPassword
+  })
+  return instance.post('/u/user/login', data)
+}
+
+export const getUserInfo = () => {
+  return instance.get('/u/user/list')
+}
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+import store from './store'
+import Axios from 'axios'
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+Vue.prototype.$axios = Axios
+Vue.prototype.HOME = '/api'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
